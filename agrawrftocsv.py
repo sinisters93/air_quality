5ebc3136c3ed169214aefee9f09872eec13e4150
sinisters93 <146200254+sinisters93@users.noreply.github.com>
2023-09-28 22:35:52 +0530

    agrawrftocsv.py

--- agrawrftocsv.py
+++ agrawrftocsv.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 11 23:43:17 2021
+This script reads data from a NetCDF file (wrfout_d01_2018-02-15_00:00:00) containing PM2.5 data,
+slices the data to extract a specific time range and spatial region, calculates the mean over time,
+and saves the resulting data as a CSV file (1_31dec.csv).
 
+Created on Thu Feb 11 23:43:17 2021
 @author: hp
 """
 
