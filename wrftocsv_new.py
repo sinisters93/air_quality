66eba27b5e626c77a01456d9dfda33fec05232c0
sinisters93 <146200254+sinisters93@users.noreply.github.com>
2023-09-29 16:49:12 +0530

    Update and rename wrftocsv (1).py to wrftocsv_new.py

--- wrftocsv_new.py
+++ wrftocsv_new.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 11 23:43:17 2021
+This Python script is designed to process meteorological data from a NetCDF file.
+It reads the file "wrfout_d01_2018-09-15_00:00:00" and extracts the variable "PM2_5_DRY."
+The script performs slicing on the n-dimensional array, calculating the mean along the time axis.
+The resulting data is then converted into a DataFrame and saved as "data7_13oct.csv."
 
+Created on Thu Feb 11 23:43:17 2021
 @author: hp
 """
-
 #! /usr/bin/python
 
 from netCDF4 import Dataset
@@ -17,14 +20,10 @@ nc1 = Dataset(file_nc, 'r+')
 ##extract variable from file
 data = nc1.variables["PM2_5_DRY"][:]
 
-
-
-
 ##slicing in n-dimensional array
 data_slice = data[528:696,0,:,:] ### time, layer, lat, lon
 
 data_mean = data_slice.mean(axis =0)
-
 
 df = pd.DataFrame(data_mean)
 df.to_csv("data7_13oct.csv",index=False)
