2b8844c6b693426932203e6efe72a345ba818a4c
sinisters93 <146200254+sinisters93@users.noreply.github.com>
2023-09-29 16:41:56 +0530

    Update test_new.py

--- test_new.py
+++ test_new.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 22 14:28:51 2021
+Description:
+This Python script is designed to manipulate a NETCDF file containing 13 levels of data. The primary purpose is to set all values to zero and assign a specific value to a given latitude and longitude location within the file. 
 
-@author: Dell
+Variables:
+- lat_station: The target latitude for assigning the value.
+- lon_station: The target longitude for assigning the value.
+- val: The value to be assigned to the specified latitude and longitude coordinates.
+- filepath: The path to the NETCDF file containing the data.
+- nc: The NETCDF dataset object used to access and modify the data.
+- emi: An array holding the 'emis_tot' variable data from the NETCDF file.
+- lat: An array holding latitude values from the NETCDF file.
+- lon: An array holding longitude values from the NETCDF file.
+
+Procedure:
+1. Load the NETCDF dataset from the specified file.
+2. Initialize the 'emi' variable with the 'emis_tot' data from the NETCDF file.
+3. Set all values in the 'emi' array to zero.
+4. Find the indices in the 'lat' and 'lon' arrays corresponding to the target latitude and longitude.
+5. Assign the specified 'val' to the 'emi' array at the identified latitude and longitude.
+6. Update the 'emis_tot' variable in the NETCDF dataset with the modified 'emi' array.
+7. Close the NETCDF dataset.
+
+Note: Ensure that the specified file path ('filepath') leads to a valid NETCDF file containing the 'emis_tot' variable and that the target latitude and longitude coordinates ('lat_station' and 'lon_station') exist within the provided data.
 """
-
-
 
 import numpy as np
 from netCDF4 import Dataset
@@ -19,25 +37,15 @@ lat_station = 22.05
 lon_station = 88.15
 val         = 123 
 
-
-filepath = "D:/Bhai/13_level_BENZENE.nc"
-
-
+filepath = "D:/sahir/13_level_BENZENE.nc"
 nc = Dataset(filepath,"r+")
-
 emi = nc.variables["emis_tot"][:]
 
-
-
 lat = nc.variables["lat"][:]
-
 lon = nc.variables["lon"][:]
-
 emi = emi*0
-
 a = np.where(lat == lat_station)[0][0]
 b = np.where(lon == lon_station)[0][0]
-
 
 for i in range(13):
     emi[i][a][b] = val
